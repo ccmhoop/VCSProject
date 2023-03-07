0164e6b05195934a27f47f0198f5a53afe82c2e0
ccmar <ccmdehoop1991@gmail.com>
2023-03-07 13:22:28 +0100

    Conner Test

---
+++ src/GitTest/GitTest.java
@@ -0,0 +1,10 @@
+package GitTest;
+
+public class GitTest {
+
+    public static void hoi (){
+
+        System.out.println("\nHallo");
+    }
+
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,10 @@
 package Main;
 
+import GitTest.GitTest;
+
 public class Main {
+    public static void main(String[] args) {
+        GitTest.hoi();
+    }
+
 }
